16281fe0395b0c0c26196ec63686e1009d13d146
Ankur Gamit <ankur.gamit@ispl.com>
2023-06-08 17:38:50 +0530

    inital commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorApp.Hubs;
+
+public class ChatHub:Hub
+{
+    public Task SendMessage(string user, string message)
+    {
+
+        return Clients.All.SendAsync("ReceiveMessage", user, message);
+
+    }
+
+}
